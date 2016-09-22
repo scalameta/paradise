fd29c54e143ca96b0d377ad57ea4dcf1277a8be0
Eugene Burmako <xeno.by@gmail.com>
2016-09-22 16:42:15 +0200

    an initial low-tech version of scala.meta AST persistence
      * Only enabled under -Dpersist.enable
      * Creates a phase that runs immediately after typer
        and converts attributed scalac ASTs to scala.meta ASTs
      * Hijacks the GenBCode with a custom backend that emits
        TASTY classfile sections corresponding to the converted scala.meta ASTs
      * Resulting TASTY payloads don't fully implement the specification yet
        Instead, they simply contain a "Scalameta" section that stores binary
        payload that results from serializing scala.meta ASTs with Java serialization
      * Can be debugged via -Dpersist.debug

--- plugin/src/main/scala/org/scalameta/paradise/Plugin.scala
+++ plugin/src/main/scala/org/scalameta/paradise/Plugin.scala
@@ -5,21 +5,32 @@ import scala.tools.nsc.plugins.{Plugin => NscPlugin, PluginComponent => NscPlugi
 import scala.collection.{mutable, immutable}
 import org.scalameta.paradise.reflect.ReflectToolkit
 import org.scalameta.paradise.converters.Converter
+import org.scalameta.paradise.converters.PersistPhase
 import org.scalameta.paradise.parser.HijackSyntaxAnalyzer
 import org.scalameta.paradise.typechecker.HijackAnalyzer
 import org.scalameta.paradise.typechecker.AnalyzerPlugins
+import org.scalameta.paradise.backend.HijackBackend
 
 class Plugin(val global: Global)
     extends NscPlugin
+    with PersistPhase
     with HijackSyntaxAnalyzer
     with HijackAnalyzer
-    with AnalyzerPlugins {
+    with AnalyzerPlugins
+    with HijackBackend {
   val name        = "macroparadise"
   val description = "Empowers production Scala compiler with latest macro developments"
-  val components  = Nil
+  val components = {
+    val persistEnabled = sys.props("persist.enable") != null
+    if (persistEnabled) List[NscPluginComponent](PersistComponent)
+    else Nil
+  }
 
   hijackSyntaxAnalyzer()
   val newAnalyzer = hijackAnalyzer()
   newAnalyzer.addAnalyzerPlugin(AnalyzerPlugin)
   newAnalyzer.addMacroPlugin(MacroPlugin)
+  val (newBackend, oldBackend) = hijackBackend()
+  // TODO: looks like it doesn't get hijacked cleanly...
+  // if (global.genBCode ne newBackend) sys.error("failed to hijack backend")
 }
